ffdfb9636e1012321468100d7ddaf21498cdd204
Owen Ambrose (dba SF) <oa227r@gmail.com>
2025-04-16 16:32:36 -0500

    more general endpoint implementations

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,13 +6,24 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (users[username]) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  users[username] = password;
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   const bookList = Object.values(books).map(book => ({
+    isbn: book.isbn,
     author: book.author,
     title: book.title,
     reviews: book.reviews
@@ -35,20 +46,53 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  // Search for partial matches
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
+
+  /* This is a strict search, I don't like this approach as much:
+  const booksByAuthor = [];
+  const bookKeys = Object.keys(books);
+  bookKeys.forEach(key => {
+    if (books[key].author.toLowerCase() === author.toLowerCase()) {
+      booksByAuthor.push(books[key]);
+    }
+  }); */
+
+  if (booksByAuthor.length === 0) {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
+
+  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+
+  if (booksByTitle.length === 0) {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
+
+  return res.status(200).json(booksByTitle);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find(book => book.isbn === isbn);
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (Object.keys(book.reviews).length === 0) {
+    return res.status(200).json({ message: "There are no reviews for this book yet" });
+  }
+
+  return res.status(200).json(book.reviews);
 });
 
 module.exports.general = public_users;
