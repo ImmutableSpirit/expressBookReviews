bdf165148ab7dda5b245b7771cac076c5b34b262
Owen Ambrose (dba SF) <oa227r@gmail.com>
2025-04-16 20:43:55 -0500

    refactored four methods using Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,64 +20,96 @@ public_users.post("/register", (req,res) => {
   return res.status(201).json({ message: "User registered successfully" });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  const bookList = Object.values(books).map(book => ({
-    isbn: book.isbn,
-    author: book.author,
-    title: book.title,
-    reviews: book.reviews
-  }));
-
-  return res.status(200).json(bookList);
+public_users.get('/', function (req, res) {
+  getBookList()
+    .then(bookList => res.status(200).json(bookList))
+    .catch(error => res.status(500).json({ message: "Failed to retrieve books" }));
 });
+
+function getBookList() {
+  return new Promise((resolve, reject) => {
+    try {
+      const bookList = Object.values(books).map(book => ({
+        isbn: book.isbn,
+        author: book.author,
+        title: book.title,
+        reviews: book.reviews
+      }));
+      resolve({ message: "Returned data using a Promise", data: bookList });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  const book = Object.values(books).find(book => book.isbn === isbn);
-
-  if (!book) {
-    return res.status(404).json({ message: "Book not found" });
-  }
-
-  return res.status(200).json(book);
+  getBookByIsbn(isbn)
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(404).json({ message: "Book not found" }));
 });
+
+function getBookByIsbn(isbn) {
+  return new Promise((resolve, reject) => {
+    try {
+      const book = Object.values(books).find(book => book.isbn === isbn);
+      if (!book) {
+        reject("Book not found");
+      } else {
+        resolve({ message: "Returned book data using a Promise", data: book });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-
-  // Search for partial matches
+public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
-
-  /* This is a strict search, I don't like this approach as much:
-  const booksByAuthor = [];
-  const bookKeys = Object.keys(books);
-  bookKeys.forEach(key => {
-    if (books[key].author.toLowerCase() === author.toLowerCase()) {
-      booksByAuthor.push(books[key]);
-    }
-  }); */
-
-  if (booksByAuthor.length === 0) {
-    return res.status(404).json({ message: "No books found by this author" });
-  }
-
-  return res.status(200).json(booksByAuthor);
+  getBooksByAuthor(author)
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(404).json({ message: "No books found by this author" }));
 });
+
+function getBooksByAuthor(author) {
+  return new Promise((resolve, reject) => {
+    try {
+      const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase().includes(author.toLowerCase()));
+      if (booksByAuthor.length === 0) {
+        reject("No books found");
+      } else {
+        resolve({ message: "Returned books by author using a Promise", data: booksByAuthor });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
-
-  if (booksByTitle.length === 0) {
-    return res.status(404).json({ message: "No books found with this title" });
-  }
-
-  return res.status(200).json(booksByTitle);
+  getBooksByTitle(title)
+    .then(response => res.status(200).json(response))
+    .catch(error => res.status(404).json({ message: "No books found with this title" }));
 });
+
+function getBooksByTitle(title) {
+  return new Promise((resolve, reject) => {
+    try {
+      const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase().includes(title.toLowerCase()));
+      if (booksByTitle.length === 0) {
+        reject("No books found");
+      } else {
+        resolve({ message: "Returned books by title using a Promise", data: booksByTitle });
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
